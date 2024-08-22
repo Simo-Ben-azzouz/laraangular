a679d2cf6af7edf460b9d0a89d01a3333ddd88bf
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-08-22 06:36:56 +0100

    create-site-controller

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index(){
+       return view('Site.accuiel');    
+    }
+
+    public function services(){
+       return view('Site.services');        
+    }
+
+    public function blog(){
+       return view('Site.blog');        
+    }
+
+    public function about(){
+       return view('Site.about');        
+    }
+
+    public function contact(){
+       return view('Site.contact');        
+    }
+}
