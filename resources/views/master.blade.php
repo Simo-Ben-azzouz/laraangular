51cfa7e46b3c6891db48055bffc065da10a8a450
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-10-21 07:28:57 +0100

    add-master-page

---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>koncrate</title>
+    <link rel="stylesheet" href="{{asset('css/bulma.css')}}">
+</head>
+<body>
+
+</body>
+@include('partials.menu')
+@yield('content')
+</html>
