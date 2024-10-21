251759590766ed166fde1b319fcf0f607a8541a6
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-10-21 07:26:36 +0100

    add-pages-with-bulma.css

---
+++ resources/views/Site/about.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+@section('content')
+<h1>about</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae deserunt temporibus ipsam at illo optio ea dignissimos praesentium quisquam numquam, impedit veniam amet sed inventore, quos expedita esse alias molestiae!</p>
+  @endsection
