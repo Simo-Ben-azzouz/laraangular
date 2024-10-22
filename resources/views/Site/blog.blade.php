7cf53f37fe2dfcb06125f855493e935ccbbe0e0c
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-10-22 06:15:26 +0100

    design-page-blog

--- resources/views/Site/blog.blade.php
+++ resources/views/Site/blog.blade.php
@@ -1,4 +1,94 @@
 @extends('master')
 @section('content')
-<h1>blog</h1>
+<section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+  <ul>
+    <li><a href="{{ url('/') }}">Home</a></li>
+    <li class="is-active"><a href="#" aria-current="page" class="has-text-black">blog</a></li>
+  </ul>
+</nav>
+        <p class="title">Our blog</p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="columns is-marginless">
+        <div class="column">
+        <div class="card">
+  <div class="card-image">
+    <figure class="image is-16by9">
+      <img
+        src="https://bulma.io/assets/images/placeholders/1280x960.png"
+        alt="Placeholder image"
+      />
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img
+            src="https://bulma.io/assets/images/placeholders/96x96.png"
+            alt="Placeholder image"
+          />
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4 has-text-white">John Smith</p>
+        <p class="subtitle is-6 has-text-white">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+      iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+      <a href="#">#responsive</a>
+      <br />
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
+        </div>
+
+        <div class="column">
+        <div class="card">
+  <div class="card-image">
+    <figure class="image is-16by9">
+      <img
+        src="https://bulma.io/assets/images/placeholders/1280x960.png"
+        alt="Placeholder image"
+      />
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img
+            src="https://bulma.io/assets/images/placeholders/96x96.png"
+            alt="Placeholder image"
+          />
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4 has-text-white">John Smith</p>
+        <p class="subtitle is-6 has-text-white">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+      iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+      <a href="#">#responsive</a>
+      <br />
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+  </div>
+</section>
+
 @endsection
