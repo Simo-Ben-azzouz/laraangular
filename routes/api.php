daa9c866fe5a3794801f80434bbdab2dd39e6a8d
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-12-03 06:37:52 +0100

    create-all-controller-for-api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/posts', [App\Http\Controllers\Api\PostsController::class, 'getAllPosts']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
