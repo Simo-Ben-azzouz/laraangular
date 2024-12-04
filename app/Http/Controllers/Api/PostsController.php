d5a2c6a4608b52ab2984f23aff85e072049b8be7
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-12-04 06:51:41 +0100

    create-method-getbyid&getlimit

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Http\Resources\PostsResource;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function getAllPosts(){
-        return Post::all();
+    public function getAllPosts($limit=null){
+        return PostsResource::collection(Post::take($limit)->get());
+    }   
+    
+    public function getPostById($id){
+        return new PostsResource(Post::find($id));
     }
 }
