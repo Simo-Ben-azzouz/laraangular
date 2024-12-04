a9c83c70bd6c78d7bfbfbad142a5683882578118
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-12-04 06:50:55 +0100

    create-PostsResources

---
+++ app/Http/Resources/PostsResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id ,
+            "category" => $this->category_id ,
+            "titre" => $this->title ,
+            "slug" => $this->slug ,
+            "contenu" => $this->body ,
+            "description" => $this->excerpte ,
+            "image" => $this->image ,
+            "dateCreation" => $this->created_at ,
+        ];
+    }
+}
